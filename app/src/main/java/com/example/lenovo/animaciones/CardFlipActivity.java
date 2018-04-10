fe8183f1d8a797c60227cbb94df396fbeab466b7
Leonardo <stenoner90@gmail.com>
2018-04-10 02:56:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/animaciones/CardFlipActivity.java
@@ -0,0 +1,124 @@
+package com.example.lenovo.animaciones;
+
+import android.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class CardFlipActivity extends AppCompatActivity
+        implements FragmentManager.OnBackStackChangedListener {
+
+    // Un objeto tipo handler para diferenciar las operaciones de la UI
+    private Handler mHandler = new Handler();
+    // Atributo para mostrar la card frontal o trasera de la UI
+    private boolean mShowingBack = false;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card);
+        if(savedInstanceState == null){
+            // Si no hay instancia previa, agregamos el fragmento que representa el frente.
+            // Si existe instancia, el fragmento será el que ya ha sido agregado previamente.
+                    getFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new CardFrontFragment())
+                    .commit();
+        }else{
+            mShowingBack = (getSupportFragmentManager().getBackStackEntryCount()
+                    > 0);
+        }
+        getSupportFragmentManager().addOnBackStackChangedListener(this);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuItem item = menu.add(Menu.NONE, R.id.action_flip, Menu.NONE,
+                mShowingBack
+                        ? R.string.action_photo
+                        : R.string.action_info);
+        item.setIcon(mShowingBack
+                ? R.drawable.ic_menu_camera
+                : R.drawable.ic_menu_manage);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
+                return true;
+            case R.id.action_flip:
+                flipCard();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void flipCard() {
+        if (mShowingBack) {
+            getFragmentManager().popBackStack();
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    mShowingBack = false;
+                    invalidateOptionsMenu();
+                }
+            });
+            return;
+        }
+        // Gira hacia atrás.
+        mShowingBack = true;
+        getFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(
+                        R.animator.card_flip_right_in, R.animator.card_flip_right_out,
+                        R.animator.card_flip_left_in, R.animator.card_flip_left_out)
+                .replace(R.id.container, new CardBackFragment())
+                .addToBackStack(null)
+                .commit();
+        mHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                invalidateOptionsMenu();
+            }
+        });
+    }
+    @Override
+    public void onBackStackChanged() {
+        mShowingBack = (getFragmentManager().getBackStackEntryCount() > 0);
+        invalidateOptionsMenu();
+    }
+
+    public static class CardFrontFragment extends Fragment {
+        public CardFrontFragment() {
+        }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_card_front, container,
+                    false);
+        }
+    }
+
+    public static class CardBackFragment extends Fragment {
+        public CardBackFragment() {
+        }
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_card_back, container,
+                    false);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/animaciones/ScreenSlideActivity.java
@@ -0,0 +1,126 @@
+package com.example.lenovo.animaciones;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class ScreenSlideActivity extends AppCompatActivity {
+
+    // Definimos el número de páginas que mostraremos
+    private static final int NUM_PAGES = 5;
+    // El widget pager, para manipular la animación y acceder a las páginas
+    private ViewPager mPager;
+    // El adaptador para el uso del pager.
+    private PagerAdapter mPagerAdapter;
+
+    private class ZoomOutPageTransformer implements ViewPager.PageTransformer {
+
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        @Override
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+            int pageHeight = view.getHeight();
+            if (position < -1) { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+            } else if (position <= 1) { // [-1,1]
+                // Modify the default slide transition to shrink the page as well
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0) {
+                    view.setTranslationX(horzMargin - vertMargin / 2);
+                } else {
+                    view.setTranslationX(-horzMargin + vertMargin / 2);
+                }
+                // Scale the page down (between MIN_SCALE and 1)
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+                // Fade the page relative to its size.
+                view.setAlpha(MIN_ALPHA +
+                        (scaleFactor - MIN_SCALE) /
+                                (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+            } else { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
+            }
+        }
+    }
+
+    private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
+        public ScreenSlidePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return ScreenSlidePageFragment.create(position);
+        }
+        @Override
+        public int getCount() {
+            return NUM_PAGES;
+        }
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_slide);
+        //Instanciamos el ViewPager y un PAgeAdapter
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPagerAdapter = new ScreenSlidePagerAdapter(getSupportFragmentManager());
+        mPager.setAdapter(mPagerAdapter);
+        mPager.setPageTransformer(true, new ZoomOutPageTransformer());
+        mPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.activity_screen_slide, menu);
+        menu.findItem(R.id.action_previous).setEnabled(mPager.getCurrentItem() > 0);
+        MenuItem item = menu.add(Menu.NONE,R.id.action_next,Menu.NONE,
+                (mPager.getCurrentItem() == mPagerAdapter.getCount() - 1)
+                        ? R.string.action_finish
+                        : R.string.action_next);
+        item.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
+                return true;
+            case R.id.action_previous:
+                mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+                return true;
+            case R.id.action_next:
+                mPager.setCurrentItem(mPager.getCurrentItem() + 1);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
+
